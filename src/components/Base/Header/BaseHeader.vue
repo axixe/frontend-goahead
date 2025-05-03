6c83b4d6b001956d1c1a9436152b643de6e05d0a
axixe <s1ngleez@yandex.ru>
2025-05-03 18:48:02 +0300

    feat(BaseHeader): create base header component

---
+++ src/components/Base/Header/BaseHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <img src="@/assets/images/logo.webp" width="101" height="70" alt="logo" />
+
+    <h1>YOUTUBE PARSE</h1>
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  align-items: center;
+  gap: $indent-xl;
+}
+</style>
