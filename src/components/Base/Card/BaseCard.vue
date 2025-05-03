5c101004c889c9f01d7e527f01fb18488245cf5a
axixe <s1ngleez@yandex.ru>
2025-05-03 18:11:05 +0300

    feat(BaseCard): create component

---
+++ src/components/Base/Card/BaseCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <slot name="card-header"></slot>
+
+    <slot name="card-main-content"></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.card {
+  display: inline-flex;
+  flex-direction: column;
+  gap: $indent-m;
+  padding: $indent-l;
+  border-radius: $border-radius;
+  background: $color-base-light;
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+}
+</style>
