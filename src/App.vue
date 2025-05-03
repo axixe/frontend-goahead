cbf2b35a5e7760e1a0a96ed89ffb4009992908b3
axixe <s1ngleez@yandex.ru>
2025-05-03 18:49:16 +0300

    feat(App): implement layout and view into app component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-
+  <DefaultLayout>
+    <IndexView />
+  </DefaultLayout>
 </template>
 
 <script setup lang="ts">
 
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import IndexView from '@/views/IndexView.vue'
 </script>
 
 <style lang="scss">
