f3f66d23a7d1f4a388fef7a52ded0dec02e6e161
axixe <s1ngleez@yandex.ru>
2025-05-03 18:47:31 +0300

    feat(DefaultLayout): create default layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="layout">
+    <BaseHeader />
+
+    <main>
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BaseHeader from '@/components/Base/Header/BaseHeader.vue'
+
+</script>
+
+<style scoped lang="scss">
+.layout {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  padding: $indent-xl 120px;
+
+  @media screen and (max-width: 991px) {
+    padding: $indent-s $indent-l;
+  }
+}
+</style>
