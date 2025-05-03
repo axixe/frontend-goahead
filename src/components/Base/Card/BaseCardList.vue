4e7fa8b094b537c6163bc110ec4129d14c2ec1d6
axixe <s1ngleez@yandex.ru>
2025-05-03 18:11:23 +0300

    feat(BaseCardList): create component

---
+++ src/components/Base/Card/BaseCardList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card-list">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.card-list {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  gap: $indent-l;
+}
+</style>
