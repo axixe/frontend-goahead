7e4e974a5b13dff2735b60ed0e9fbf8362e27f0c
axixe <s1ngleez@yandex.ru>
2025-05-05 19:21:10 +0300

    feat(BaseSelectOption): create component

---
+++ src/components/Base/Select/BaseSelectOption.vue
@@ -0,0 +1,31 @@
+<template>
+  <li class="select-option" @click="$emit('select:element', query)">
+    <slot></slot>
+  </li>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  query: string
+}
+
+withDefaults(defineProps<Props>(), {
+  query: '',
+})
+
+defineEmits<{
+  (e: 'select:element', value: string): void
+}>()
+</script>
+
+<style scoped lang="scss">
+.select-option {
+  padding: $indent-s $indent-l;
+  transition: background 0.3s ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    background: $color-base;
+  }
+}
+</style>
