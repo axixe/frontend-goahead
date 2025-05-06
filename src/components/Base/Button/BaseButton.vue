3cac90caf941801fa8d26f2b44ced14ca5e86d0f
axixe <s1ngleez@yandex.ru>
2025-05-06 21:23:30 +0300

    feat(BaseButton): create component

---
+++ src/components/Base/Button/BaseButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="button" type="button" :disabled="disabled">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  disabled?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  disabled: false,
+})
+</script>
+
+<style scoped lang="scss">
+.button {
+  padding: 12px;
+  border-radius: $border-radius;
+  background: $color-base-light;
+  color: $color-white;
+}
+</style>
