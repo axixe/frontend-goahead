d5f2e46aa7432980dfbc868b6b671dc12c0ad30f
axixe <s1ngleez@yandex.ru>
2025-05-07 13:51:29 +0300

    feat(index): add sort in view

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="index-page">
-    <BaseForm class="index-page__search-form" @form:submit="updateUrlQuery('q', formModel)">
+    <BaseForm class="index-page__search-form" @form:submit="router.push({ query: { q: formModel } })">
       <BaseInput
         v-model="formModel"
         :name="'video-name'"
         :label="'I GONNA WATCH...'"
         class="index-page__input"
       />
-      <SearchButton :disabled="isLoading" @click="updateUrlQuery('q', formModel)" />
+      <SearchButton :disabled="isLoading" @click="router.push({ query: { q: formModel } })" />
     </BaseForm>
+
+    <BaseSelect
+        v-model="sortModel"
+      :options="DateSortSelectMock"
+      :name="'date-sort'"
+      :label="'Sort by'"
+      :disabled="isLoading"
+      @select:element="updateUrlQuery('order', sortModel)"
+    />
 
     <BasePreloader v-if="isLoading" />
     <VideoList
@@ -28,26 +37,30 @@ import BaseInput from "@/components/Base/Input/BaseInput.vue";
 import SearchButton from "@/components/ui/SearchButton.vue";
 import BasePreloader from "@/components/Base/Preloader/BasePreloader.vue";
 import VideoList from "@/components/ui/VideoList.vue";
+import BaseSelect from "@/components/Base/Select/BaseSelect.vue";
 import useVideoSearch from "@/app/composables/useVideoSearch.ts";
 import { useUpdateUrlQuery } from "@/composables/useUpdateUrlQuery.js";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+import { DateSortSelectMock } from "@/mock/DateSortSelectMock.ts";
 
 const { search, pageTokens, foundedVideos, isLoading } = useVideoSearch()
 const { updateUrlQuery } = useUpdateUrlQuery()
 
-const formModel = ref<string>('')
-
 const route = useRoute()
+const router = useRouter()
+
+const formModel = ref<string>('')
+const sortModel = ref<string>('')
 
 watch(
   () => route.query,
   async (newQuery: any) => {
     const formattedQueryLink = new URLSearchParams(newQuery).toString()
 
-    await search(formattedQueryLink);
+    await search(formattedQueryLink)
   },
   { immediate: true }
-);
+)
 </script>
 
 <style scoped lang="scss">
